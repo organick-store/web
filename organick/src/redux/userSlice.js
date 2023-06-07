51056e836618df287ca81cba95ad30422641752c
Nazofer <nvovk.2004@gmail.com>
2023-06-07 18:03:32 +0300

    add ordering logic

--- organick/src/redux/userSlice.js
+++ organick/src/redux/userSlice.js
@@ -1,16 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 import AuthService from '../services/AuthService';
-const initialUserState = { name: '', email: '',  isAuth: false};
+const initialUserState = { name: '', email: '', address: '', isAuth: false };
 
 export const userSlice = createSlice({
   name: 'user',
   initialState: initialUserState,
   reducers: {
     setUser(state, action) {
-      if (action.payload.name) {
-        state.name = action.payload.name
-      }
+      state.name = action.payload.name;
       state.email = action.payload.email;
+      state.address = action.payload.address;
     },
     setAuth(state, action) {
       state.isAuth = action.payload;
@@ -23,35 +22,52 @@ export const { setUser, setAuth } = userSlice.actions;
 export const login = (email, password) => async (dispatch) => {
   try {
     const response = await AuthService.login(email, password);
-    console.log(response);
     localStorage.setItem('token', response.data.token);
     if (response.data.status === 'Success') {
-      console.log('success');
       dispatch(setAuth(true));
-      dispatch(setUser({email: response.data.email, name: response.data.name}));
-      return {message: 'Successfully authorized'}
+      dispatch(
+        setUser({
+          email: response.data.email,
+          name: response.data.name,
+          address: response.data.address,
+        })
+      );
+      return { message: 'Successfully authorized' };
     }
-    return {message: response.data.message || 'Something went wrong'}
+    return { message: response.data.message || 'Something went wrong' };
   } catch (e) {
     console.log(e);
   }
 };
 
-export const registration = (name, email, password, phone, address) => async (dispatch) => {
-  try {
-    const response = await AuthService.registration(name, email, password, phone, address);
-    console.log(response);
-    localStorage.setItem('token', response.data.token);
-    if (response.data.status === 'Success') {
-      dispatch(setAuth(true));
-      dispatch(setUser({email: response.data.email, name: response.data.name}));
-      return { success: true, message: 'Registration successful.' };
+export const registration =
+  (name, email, password, phone, address) => async (dispatch) => {
+    try {
+      const response = await AuthService.registration(
+        name,
+        email,
+        password,
+        phone,
+        address
+      );
+      console.log(response);
+      localStorage.setItem('token', response.data.token);
+      if (response.data.status === 'Success') {
+        dispatch(setAuth(true));
+        dispatch(
+          setUser({
+            email: response.data.email,
+            name: response.data.name,
+            address: response.data.address,
+          })
+        );
+        return { success: true, message: 'Registration successful.' };
+      }
+      return { success: false, message: 'Registration failed.' };
+    } catch (e) {
+      console.error(e);
     }
-    return { success: false, message: 'Registration failed.' };
-  } catch (e) {
-    console.error(e);
-  }
-};
+  };
 
 export const refresh = () => async (dispatch) => {
   try {
@@ -61,7 +77,13 @@ export const refresh = () => async (dispatch) => {
     console.log(response);
     if (response.data.status === 'Success') {
       dispatch(setAuth(true));
-      dispatch(setUser({email: response.data.email, name: response.data.name}));
+      dispatch(
+        setUser({
+          email: response.data.email,
+          name: response.data.name,
+          address: response.data.address,
+        })
+      );
     } else {
       localStorage.removeItem('token');
     }
@@ -74,11 +96,10 @@ export const activate = (token) => async (dispatch) => {
   try {
     if (!token) return;
     const response = await AuthService.activate(token);
-    console.log(response);
     if (response.data.status === 'Success') {
-      return {message: response.data.message}
+      return { message: response.data.message };
     }
-    return {message: response.data.message}
+    return { message: response.data.message };
   } catch (e) {
     console.log(e);
   }
@@ -88,9 +109,9 @@ export const logout = () => async (dispatch) => {
   try {
     localStorage.removeItem('token');
     dispatch(setAuth(false));
-    dispatch(setUser({name: '', email: ''}));
+    dispatch(setUser({ name: '', email: '' }));
   } catch (e) {
-    console.log(e.response?.data?.message);
+    console.log(e);
   }
 };
 

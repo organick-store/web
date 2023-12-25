0aed94b1a29c55d9295cb337a03a74c874bc8bd1
Nazik <nvovk.2004@gmail.com>
2023-12-25 23:48:48 +0200

    fix: product cards discount condition

--- src/components/sections/products/product-card/product-price/product-price.js
+++ src/components/sections/products/product-card/product-price/product-price.js
@@ -5,7 +5,7 @@ import classNames from 'classnames';
 const ProductPrice = ({ product, className }) => {
   return (
     <span className={classNames(styles.price, className)}>
-      {product?.discount && <span className={classNames(styles.discount, className)}>
+      {product?.discount > 0 && <span className={classNames(styles.discount, className)}>
         ${product?.price.toFixed(2)}
       </span>}
       ${(product?.price - (product?.discount || 0)).toFixed(2)}
